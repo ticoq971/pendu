53a421c88b63dea2284db4e12706540d2a57af13
Tristan aka Desperados <ticoq97.1@outlook.com>
2025-02-17 14:24:34 +0100

    pendu

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+<><section className='container'>
+
+
+
+<input type="text" 
+/><button> Click me</button>
+
+<div className='pendu'>
+<h1> Pendu</h1>
+<img src="" alt="" />
+</div>
+
+<input type="text" /><button> Click me</button>
+</section>
+</>
+   
+  )
+}
+
+export default App
